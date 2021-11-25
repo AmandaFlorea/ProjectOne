cf846d3affa3cc31c130474bb6f00b0965721dd5
agg72 <agg72@DESKTOP-BN6NDO2>
2021-11-24 19:58:23 -0500

    added method

--- src/one/DemoOne.java
+++ src/one/DemoOne.java
@@ -3,6 +3,14 @@ package one;
 public class DemoOne {
 	public static void main(String[] args) {
 		System.out.println("Demo making sure code was pushed to the repository");
+		
+		
+	
 	}
+	
+	public static void addNum(int a, int b ) {
+		System.out.println(a + b);
+	}
+	
 
 }
